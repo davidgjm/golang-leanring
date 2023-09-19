c4e49c4381725e04bea4300677ee492bb93df6ca
David <dvdgjm@hotmail.com>
2023-09-19 23:15:25 +0800

    example code with exercises from gopl

---
+++ gopl.io/ch1/lissajous/lissajous.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White,
+	color.RGBA{0x00, 0xff, 0x00, 0xFF},
+	color.RGBA{0x0d, 0x6e, 0xfd, 0xff},
+	color.RGBA{0x66, 0x10, 0xf2, 0xff},
+	color.RGBA{0xd6, 0x33, 0x84, 0xff},
+}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 400
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.1; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(len(palette))))
+
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+
+}
