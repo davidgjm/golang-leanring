f31b88293ee86cdc6dd7da2ade7326f1e0f7ad40
David Gao <dvdgjm@hotmail.com>
2024-10-03 22:30:13 +0800

    finished section 1.4

--- gopl.io/ch1/exercise/exercise1_5.go
+++ gopl.io/ch1/exercise/exercise1_5.go
@@ -10,20 +10,14 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White,
-	color.RGBA{0x00, 0xff, 0x00, 0xFF},
-	color.RGBA{0x0d, 0x6e, 0xfd, 0xff},
-	color.RGBA{0x66, 0x10, 0xf2, 0xff},
-	color.RGBA{0xd6, 0x33, 0x84, 0xff},
-}
+var palette = []color.Color{color.White, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	whiteIndex = 0 /// first color in palette
+	blackIndex = 1 //next color in palette
 )
 
 func main() {
-
 	lissajous(os.Stdout)
 }
 
@@ -31,27 +25,24 @@ func lissajous(out io.Writer) {
 	const (
 		cycles  = 5
 		res     = 0.001
-		size    = 400
+		size    = 100
 		nframes = 64
 		delay   = 8
 	)
-
 	freq := rand.Float64() * 3.0
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.1; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(len(palette))))
-
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(out, &anim)
-
 }
