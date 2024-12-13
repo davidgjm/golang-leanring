a7ab292ab8f5901d097e51061bd301be5ae0fddc
David Gao <david.gao@gm.com>
2024-12-13 18:49:39 +0800

    gopl- 1.6

--- gopl.io/ch1/exercise/exercise1_8.go
+++ gopl.io/ch1/exercise/exercise1_8.go
@@ -1,3 +1,4 @@
+// Fetch prints the content found at a URL
 package main
 
 import (
@@ -9,28 +10,22 @@ import (
 )
 
 func main() {
-
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "http://") {
 			url = "http://" + url
 		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 		b, err := io.ReadAll(resp.Body)
-		//b, err := io.Copy(os.Stdout, resp.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: io copy failed: %v\n", err)
-		}
 		resp.Body.Close()
-
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("status: %s\n", resp.Status)
 		fmt.Printf("%s", b)
 	}
 }
--- gopl.io/ch1/fetch/go.mod
+++
@@ -1,3 +0,0 @@
-module gopl.io/ch1/fetch
-
-go 1.21.1
